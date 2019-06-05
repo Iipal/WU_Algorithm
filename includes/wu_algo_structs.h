81284b24c77e169cf010e52512fec9bed73f8826
Iipal <malkoleyplay@gmail.com>
2019-06-05 20:09:16 +0300

    added attribute vectors and Time struct

--- includes/wu_algo_structs.h
+++ includes/wu_algo_structs.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 18:27:41 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/05 18:30:33 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/05 20:05:54 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,19 @@
 # include <SDL2/SDL.h>
 # include <SDL2/SDL_ttf.h>
 
-typedef struct	s_sdl
-{
+typedef int32_t  Dot __attribute__((vector_size(8),aligned));
+typedef double_t fDot __attribute__((vector_size(16),aligned));
+typedef double_t Vector __attribute__((vector_size(32),aligned));
+
+typedef struct	s_time {
+	float_t	old;
+	float_t	current;
+	float_t	res;
+	float_t	ms;
+	float_t	fps;
+}				Time;
+
+typedef struct	s_sdl {
 	SDL_Window	*w;
 	SDL_Surface	*wsurf;
 	uint32_t	*pxls;
@@ -25,9 +36,9 @@ typedef struct	s_sdl
 	TTF_Font	*font;
 }				Sdl;
 
-typedef struct	s_environment
-{
-	Sdl	sdl;
+typedef struct	s_environment {
+	Sdl		sdl;
+	Time	fps;
 }				Environment;
 
 #endif
