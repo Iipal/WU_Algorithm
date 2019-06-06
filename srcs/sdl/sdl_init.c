aab89db99fabffecdb0414b7d8cc54c7cc078852
Iipal <malkoleyplay@gmail.com>
2019-06-06 22:50:01 +0300

    fixed sdl_init.c to correct SDL_Init call xD

--- srcs/sdl/sdl_init.c
+++ srcs/sdl/sdl_init.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 18:20:33 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/06 18:18:22 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/06 22:09:16 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ bool	sdl_init(Sdl *restrict const sdl,
 				const char *title,
 				const uint16_t w,
 				const uint16_t h) {
-	IF_VAMSG(0 > SDL_GetError(), false, "%s\n", SDL_GetError());
+	IF_VAMSG(0 > SDL_Init(SDL_INIT_EVENTS), false, "%s\n", SDL_GetError());
 	IF_NOT_VAMSG(sdl->w = SDL_CreateWindow(title,
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		w, h, SDL_WINDOW_SHOWN), false, "%s\n", SDL_GetError());
