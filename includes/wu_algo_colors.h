efa315621e00d2d371bb6a25c9eb7a332e0c3b9b
Iipal <malkoleyplay@gmail.com>
2019-06-06 23:03:11 +0300

    putplx_plot, improved

--- includes/wu_algo_colors.h
+++ includes/wu_algo_colors.h
@@ -6,13 +6,14 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:28:45 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/06 19:14:30 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/06 22:56:26 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WU_ALGO_COLORS_H
 # define WU_ALGO_COLORS_H
 
+# include "wu_algo_macroses.h"
 # include <stdint.h>
 # include <math.h>
 
@@ -34,19 +35,35 @@ typedef struct	s_rgb {
 
 typedef union	u_color {
 	uint32_t	hex;
-	RGB		c;
+	RGB			c;
 }	Color;
 
-__header_always_inline Color
-clrs_bright_inc(Color clr1, const Color clr2, const float_t percent) {
-	Color	out;
+Color	g_bg_clr;
+Color	g_fg_clr;
 
-	out = clr1;
+__wu_always_inline Color
+clrs_bright_inc(Color clr1, const Color clr2, const float_t percent) {
+	Color	out = clr1;
+
 	if (.0f < percent)
 		out.c = (RGB) { INRANGE(clr1.c.r + (clr2.c.r * percent)),
 						INRANGE(clr1.c.g + (clr2.c.g * percent)),
 						INRANGE(clr1.c.b + (clr2.c.b * percent)) };
 	return (out);
+}
+
+__wu_always_inline void
+putpxl_plot(const uint64_t x, const uint64_t y,
+	const double_t c, Uint32 *restrict const pxls) {
+
+	if (0.0f <= c && 1.0f >= c)
+		if (x && y && WIN_X > x && WIN_Y > y) {
+			Color	pxl_clr = clrs_bright_inc(CLR_BLACK, g_fg_clr, c);
+			Color	orig = {pxls[y * WIN_X + x]};
+			if (1.0f > c && orig.hex)
+				pxl_clr = clrs_bright_inc(pxl_clr, orig, 1.0f - c);
+			pxls[y * WIN_X + x] = pxl_clr.hex;
+		}
 }
 
 #endif
