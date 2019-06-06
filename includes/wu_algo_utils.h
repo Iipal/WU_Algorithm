d7f47e3af72b5c91ed812e70383ad6d75cbb8d32
Iipal <malkoleyplay@gmail.com>
2019-06-06 23:01:58 +0300

    Used inline macro. Moved & improved u_plot to wu_algo_colors.h:putpxl_plot()

--- includes/wu_algo_utils.h
+++ includes/wu_algo_utils.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:34:15 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/06 19:17:24 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/06 22:39:11 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,21 @@
 # include "wu_algo_macroses.h"
 # include "wu_algo_colors.h"
 
-# include <stdbool.h>
-# include <SDL2/SDL.h>
-
-Color	g_bg_clr;
-Color	g_fg_clr;
-
-__header_always_inline void
-u_plot(unsigned long long x, unsigned long long y,
-	double_t c, Uint32 *restrict const pxls) {
-	1.0f < c ? (c = 1.0f) : c;
-	if (0.0f <= c)
-		if (x && y && WIN_X > x && WIN_Y > y)
-			pxls[y * WIN_X + x] =
-				clrs_bright_inc(g_bg_clr, g_fg_clr, c).hex;
-}
-
-__header_always_inline double_t
+__wu_always_inline double_t
 u_ipart(double_t x) { return floor(x); }
 
-__header_always_inline double_t
+__wu_always_inline double_t
 u_round(double_t x) { return u_ipart(x + 0.5f); }
 
-__header_always_inline double_t
+__wu_always_inline double_t
 u_fpart(double_t x) { return x - floor(x); }
 
-__header_always_inline double_t
+__wu_always_inline double_t
 u_rfpart(double_t x) { return 1 - u_fpart(x); }
 
 // formula for finding the length of the segment in two-dimensional coordinates
 //	A = √(X²+Y²) = √((X2-X1)²+(Y2-Y1)²).
-__header_always_inline double_t
+__wu_always_inline double_t
 u_line_len(__v2df start, __v2df end) {
 	const double_t	x = end[0] - start[0];
 	const double_t	y = end[1] - start[1];
